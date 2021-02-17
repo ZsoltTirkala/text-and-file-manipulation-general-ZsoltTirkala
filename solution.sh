6efce35ce7f98ec7026945edcf12459954df8cfa
Szak0 <sattilabence@gmail.com>
2021-02-17 15:11:47 +0100

    Get employees with mobile number

--- solution.sh
+++ solution.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo -e "\e[1;33m Employee names \n================"
 jq '.[][] | .firstName'  employees.json
-echo -e "\n\e[1;33m Employee names with contacts \n=============================="
+echo -e "\n\e[1;33m Employees with contacts \n=============================="
 jq '.[][] | .firstName, .phoneNumbers'  employees.json
+echo -e "\n\e[1;33m Employee with mobile number \n=============================="
+jq '.[][] | .firstName, if .phoneNumbers[0].type=="home" then del(.phoneNumbers[0]) | .phoneNumbers[] else .phoneNumbers[] end' employees.json 
+
