45ac5ab6db3e6a9965570e959f90be3be4886817
ZsoltTirkala <tirkala.zsolt@gmail.com>
2021-02-17 13:21:01 +0100

    Create grep to get only POST methodes

--- texts.sh
+++ texts.sh
@@ -4,3 +4,5 @@
 sed -i.bak -E 's/\b([0-9]{1,3}\.){3}([0-9]{1,3})\b/XXX.ZZZ.YYY.\2/' access.log
 #gets the request type, url and http version
 awk '{print $6,$7,$8}' access.log
+#grep will get us only those, whose method is POST
+grep POST access.log
