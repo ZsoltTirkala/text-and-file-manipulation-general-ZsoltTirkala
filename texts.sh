bf25c8bb930b48a0d3bb75e52d59f88cf64fc2b4
ZsoltTirkala <tirkala.zsolt@gmail.com>
2021-02-17 13:33:10 +0100

    Create head and tail command for 11-20 line

--- texts.sh
+++ texts.sh
@@ -6,3 +6,5 @@ sed -i.bak -E 's/\b([0-9]{1,3}\.){3}([0-9]{1,3})\b/XXX.ZZZ.YYY.\2/' access.log
 awk '{print $6,$7,$8}' access.log
 #grep will get us only those, whose method is POST
 grep POST access.log
+#head will get the first 20 and tail will get the last 10 of that => lines (11-20)
+head -n 20 access.log | tail
